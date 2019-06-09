7402bca6a9ba343da86316bccd103d9e5aecb63f
YouDa-Cai <superfight2002@gmail.com>
2019-06-10 01:58:24 +0800

    增加 Occurrences After Bigram (解完) 增加 Maximum Product of Word Lengths (待解)

---
+++ 5083. Occurrences After Bigram/5083. Occurrences After Bigram/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace _5083._Occurrences_After_Bigram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            var result = solution.FindOcurrences("we will we will rock you", "we", "will");
+            
+        }
+    }
+
+    public class Solution
+    {
+        public string[] FindOcurrences(string text, string first, string second)
+        {
+            List<string> result = new List<string>();
+            Regex regex = new Regex($@"{first} {second} [a-z]+");
+
+            Match matched = null;
+
+            while ((matched = regex.Match(text)).Success)
+            {
+                string[] splited = matched.Captures[0].Value.Split(' ');
+                result.Add(splited[2]);
+
+                int len = text.IndexOf(matched.Captures[0].Value) + matched.Captures[0].Value.Length - splited[2].Length;
+                text = text.Remove(0, len);
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ Maximum Product of Word Lengths/Maximum Product of Word Lengths/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+namespace Maximum_Product_of_Word_Lengths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            string[] words = new string[] { };
+            var result = solution.MaxProduct(words);
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxProduct(string[] words)
+        {
+            return 1;
+        }
+    }
+}
