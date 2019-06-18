806f865c657341a83d3b7f74761041ab94cf3f0d
youda <youda.tsai@ark88.net>
2019-06-18 14:02:06 +0800

    59. Spiral Matrix II

---
+++ 318. Maximum Product of Word Lengths/318. Maximum Product of Word Lengths-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _318._Maximum_Product_of_Word_Lengths_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            string[] words = new string[] { "a", "aa", "aaa", "aaaa" };
+            //string[] words = new string[] { "a", "ab", "abc", "d", "cd", "bcd", "abcd" };
+            //string[] words = new string[] { "abcw", "baz", "foo", "bar", "xtfn", "abcdef" };
+            var result = solution.MaxProduct(words);
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Solution
+    {
+        int[] mask = new int[26];
+
+        public int MaxProduct(string[] words)
+        {
+            int n = words.Length;
+            int[] die
+            for(int i = 1; i <= words.Length; i++)
+            {
+
+            }
+        }
+
+        public int Mask()
+        {
+
+        }
+    }
+}
---
+++ 59. Spiral Matrix II/59. Spiral Matrix II/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _59._Spiral_Matrix_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int n = 1;
+            var result = solution.GenerateMatrix(n);
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Solution
+    {
+        public int[][] GenerateMatrix(int n)
+        {
+            int[][] result = new int[n][];
+            for(int i = 0; i <n; i++)
+            {
+                result[i] = new int[n];
+            }
+            int upLimit = 0, downLimit = n - 1, leftLimit = 0, rightLimit = n - 1;
+            int val = 0;
+
+            while (true)
+            {
+                for (int i = leftLimit; i <= rightLimit; i++)
+                {
+                    result[leftLimit][i] = ++val;
+                }
+                if (++upLimit > downLimit) break;
+
+                for (int i = upLimit; i <= downLimit; i++)
+                {
+                    result[i][rightLimit] = ++val;
+                }
+                if (--rightLimit < leftLimit) break;
+
+                for (int i = rightLimit; i >= leftLimit; i--)
+                {
+                    result[downLimit][i] = ++val;
+                }
+                if (--downLimit < upLimit) break;
+
+                for(int i = downLimit; i >= upLimit; i--)
+                {
+                    result[i][leftLimit] = ++val;
+                }
+                if (++leftLimit > rightLimit) break;
+            }
+
+            return result;
+        }
+    }
+}
